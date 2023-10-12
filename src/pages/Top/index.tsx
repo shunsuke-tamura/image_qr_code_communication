3cf0e1dc7d90305cf952d8c1d7e9d3b13d7a7d9c
shunsuke-tamura <wwwxx4869@gmail.com>
2023-10-13 04:07:33 +0900

    update: create TopPage

--- src/pages/Top/index.tsx
+++ src/pages/Top/index.tsx
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import "./style.css";
+
 const TopPage = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>TopPage</h1>
+      <button className="toQr" onClick={() => navigate("/to-qr")}>
+        to-qr
+      </button>
+      <button className="toImage" onClick={() => navigate("/to-image")}>
+        to-image
+      </button>
     </div>
   );
 };
