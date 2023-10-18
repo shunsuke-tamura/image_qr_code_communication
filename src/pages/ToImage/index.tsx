5d02e7fe223750acfa3e199f199ef6e403f0f25f
shunsuke-tamura <wwwxx4869@gmail.com>
2023-10-19 03:23:54 +0900

    update: recording

--- src/pages/ToImage/index.tsx
+++ src/pages/ToImage/index.tsx
@@ -1,11 +1,44 @@
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import { useZxing } from "react-zxing";
 import "./style.css";
+import Webcam from "react-webcam";
 
 const ToImagePage = () => {
   const [scan, setScan] = useState(true);
   const [image, setImage] = useState("");
   const [qrCodeDatas, setQrCodeDatas] = useState<string[]>([]);
+  const webcamRef = useRef<Webcam>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const [capturing, setCapturing] = useState(false);
+  const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
+
+  const handleDataAvailable = useCallback(
+    ({ data }: BlobEvent) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => [...prev, data]);
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStartCaptureClick = useCallback(() => {
+    if (webcamRef.current === null || webcamRef.current.stream === null) return;
+    setCapturing(true);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: "video/webm",
+    });
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
+
+  const handleStopCaptureClick = useCallback(() => {
+    if (mediaRecorderRef.current === null) return;
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+  }, [mediaRecorderRef]);
 
   const { ref } = useZxing({
     paused: !scan,
@@ -44,7 +77,17 @@ const ToImagePage = () => {
   return (
     <div>
       {/* <div className="modal-container"> */}
-      <video ref={ref}></video>
+      {/* <video ref={ref}></video> */}
+      <Webcam audio={false} ref={webcamRef}></Webcam>
+      {capturing ? (
+        <button onClick={handleStopCaptureClick} className="primary">
+          Stop
+        </button>
+      ) : (
+        <button onClick={handleStartCaptureClick} className="secondary">
+          Start
+        </button>
+      )}
       {/* </div> */}
       {/* <div style={{ zIndex: 1 }}> */}
       <div>
