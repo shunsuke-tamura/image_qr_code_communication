1c337b7fccbf01b8b31f6dc611367960bb2a9edc
shunsuke-tamura <wwwxx4869@gmail.com>
2023-12-04 05:43:05 +0900

    update: 親子関係を色で表現

--- src/pages/ToImageFromCCC/index.tsx
+++ src/pages/ToImageFromCCC/index.tsx
@@ -36,17 +36,26 @@ const ToImageFromCCCPage = () => {
       binary,
       contours,
       hierarchy,
-      cv.RETR_LIST,
+      cv.RETR_CCOMP, // RETR_EXTERNAL, RETR_LIST, RETR_CCOMP, RETR_TREE,
       cv.CHAIN_APPROX_SIMPLE
     );
     console.log("contours.size(): " + contours.size());
     // draw contours
+    const colors = [
+      new cv.Scalar(255, 0, 0, 255),
+      new cv.Scalar(255, 255, 255, 255),
+    ];
     for (let i = 0; i < contours.size(); ++i) {
-      const color = new cv.Scalar(
-        Math.round(Math.random() * 255),
-        Math.round(Math.random() * 255),
-        Math.round(Math.random() * 255)
-      );
+      // const color = new cv.Scalar(
+      //   Math.round(Math.random() * 255),
+      //   Math.round(Math.random() * 255),
+      //   Math.round(Math.random() * 255)
+      // );
+      const color = colors[hierarchy.intPtr(0, i)[3] !== -1 ? 1 : 0];
+      // if (hierarchy.intPtr(0, i)[3] === -1) {
+      //   continue;
+      // }
+      // console.log(hierarchy.intPtr(0, i)[3]);
       cv.drawContours(dst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
     }
     // show result
