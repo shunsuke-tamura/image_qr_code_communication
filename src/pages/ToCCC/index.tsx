314d6839abc1945f5cfdeff4c28f91227f241129
shunsuke-tamura <wwwxx4869@gmail.com>
2023-12-04 02:21:44 +0900

    update: 表示サンプル作成

--- src/pages/ToCCC/index.tsx
+++ src/pages/ToCCC/index.tsx
@@ -1,42 +1,25 @@
 import "./style.css";
 
+import { charList } from "../../constants";
+
+const colorBlack = "#000000";
+
 const ToCCCPage = () => {
   return (
     <div className="bg">
       <h1 className="primary-text">ToCCCPage</h1>
       <div className="ccc-container">
-        <div className="row">
-          {[...Array(15)].map(() => (
-            <div className="code-container">
-              <a className="code">A</a>
-            </div>
-          ))}
-        </div>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
-        <a className="code">B</a>
+        {charList.map((char) => (
+          <div className="row">
+            {[...Array(25)].map(() => (
+              <div className="code-container">
+                <a className="code" style={{ color: colorBlack }}>
+                  {char}
+                </a>
+              </div>
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
