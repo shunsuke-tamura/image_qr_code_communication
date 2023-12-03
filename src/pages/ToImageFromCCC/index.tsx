0d7de1e4aa9bca94fe9f467c5a96319999155616
shunsuke-tamura <wwwxx4869@gmail.com>
2023-12-04 02:56:14 +0900

    update: opencv使えるようになった

--- src/pages/ToImageFromCCC/index.tsx
+++ src/pages/ToImageFromCCC/index.tsx
@@ -1,7 +1,33 @@
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    cv: any;
+  }
+}
+
 const ToImageFromCCCPage = () => {
+  const cv = window.cv;
+
+  const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const img = new Image();
+      img.onload = () => {
+        const mat = cv.imread(img);
+        cv.imshow("output", mat);
+        mat.delete();
+      };
+      img.src = URL.createObjectURL(e.target.files[0]);
+    }
+  };
+
   return (
     <div>
       <h1>to-image-from-ccc</h1>
+
+      <div>
+        <input type="file" onChange={onChangeFile} />
+      </div>
+      <canvas id="output" />
     </div>
   );
 };
