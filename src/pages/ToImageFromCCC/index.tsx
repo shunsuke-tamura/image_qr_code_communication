9b8d2007db1dd1b50699c28efa70bdfa071af1f3
shunsuke-tamura <wwwxx4869@gmail.com>
2023-12-04 07:33:27 +0900

    update: charで切り取りできた

--- src/pages/ToImageFromCCC/index.tsx
+++ src/pages/ToImageFromCCC/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 declare global {
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -7,6 +9,9 @@ declare global {
 
 const ToImageFromCCCPage = () => {
   const cv = window.cv;
+
+  const [charImageStrList, setCharImageStrList] = useState<string[]>([]);
+  const [show, setShow] = useState<boolean>(false);
 
   const onChangeFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
@@ -20,7 +25,21 @@ const ToImageFromCCCPage = () => {
     }
   };
 
+  const cropImage = (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    src: any,
+    rect: { x: number; y: number; width: number; height: number }
+  ) => {
+    const dst = src.roi(rect);
+    const croppedCanvas = document.createElement("canvas");
+    cv.imshow(croppedCanvas, dst);
+    const croppedImageStr = croppedCanvas.toDataURL("image/png");
+    dst.delete();
+    return croppedImageStr;
+  };
+
   const executeFindContours = () => {
+    setCharImageStrList([]);
     const src = cv.imread("input");
     const gray = new cv.Mat();
     const binary = new cv.Mat();
@@ -45,18 +64,32 @@ const ToImageFromCCCPage = () => {
       new cv.Scalar(255, 0, 0, 255),
       new cv.Scalar(255, 255, 255, 255),
     ];
+    // a.最後[contour] -> b.その親[contour] -> c.prev[int] = codeContainerIdx
+    const a = hierarchy.intPtr(0, contours.size() - 1);
+    const b = hierarchy.intPtr(0, a[3]);
+    const c = b[1];
+    const codeContainerIdx = c;
     for (let i = 0; i < contours.size(); ++i) {
-      // const color = new cv.Scalar(
-      //   Math.round(Math.random() * 255),
-      //   Math.round(Math.random() * 255),
-      //   Math.round(Math.random() * 255)
-      // );
       const color = colors[hierarchy.intPtr(0, i)[3] !== -1 ? 1 : 0];
+
       // if (hierarchy.intPtr(0, i)[3] === -1) {
-      //   continue;
-      // }
-      // console.log(hierarchy.intPtr(0, i)[3]);
+      if (hierarchy.intPtr(0, i)[3] !== codeContainerIdx) {
+        // if (i !== 2008) {
+        continue;
+      }
+      console.log(
+        i,
+        hierarchy.intPtr(0, i)[0],
+        hierarchy.intPtr(0, i)[1],
+        hierarchy.intPtr(0, i)[2],
+        hierarchy.intPtr(0, i)[3]
+      );
       cv.drawContours(dst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+
+      // trim
+      const rect = cv.boundingRect(contours.get(i));
+      const croppedImageStr = cropImage(src, rect);
+      setCharImageStrList((prev) => [...prev, croppedImageStr]);
     }
     // show result
     cv.imshow("result", dst);
@@ -66,6 +99,15 @@ const ToImageFromCCCPage = () => {
     binary.delete();
     contours.delete();
     hierarchy.delete();
+  };
+
+  // 配列をn個ずつに分割する
+  const splitArray = <T,>(array: T[], n: number): T[][] => {
+    const result = [];
+    for (let i = 0; i < array.length; i += n) {
+      result.push(array.slice(i, i + n));
+    }
+    return result;
   };
 
   return (
@@ -89,6 +131,28 @@ const ToImageFromCCCPage = () => {
       <canvas id="binary" />
       <h3>result</h3>
       <canvas id="result" />
+
+      <div>
+        <button
+          className={show ? "secondary" : "primary"}
+          onClick={() => setShow((prev) => !prev)}
+        >
+          {show ? "Hide" : "Show"}
+        </button>
+      </div>
+      {show && (
+        <div>
+          {splitArray(charImageStrList, 25).map((charImageStrList) => (
+            <div style={{ display: "flex" }}>
+              {charImageStrList.map((charImageStr) => (
+                <div>
+                  <img style={{ margin: "2px" }} src={charImageStr} />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
