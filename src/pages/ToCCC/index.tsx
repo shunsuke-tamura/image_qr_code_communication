6ab66478ac5a4b79466b59270c1e37184fc64682
shunsuke-tamura <wwwxx4869@gmail.com>
2023-12-07 23:38:29 +0900

    update: 単語で括ったり、文章で括ったりを表示

--- src/pages/ToCCC/index.tsx
+++ src/pages/ToCCC/index.tsx
@@ -1,6 +1,12 @@
 import "./style.css";
 
-import { bgColorList, charList } from "../../constants";
+import {
+  bgColorList,
+  charList,
+  subjectList,
+  verbList,
+  objectList,
+} from "../../constants";
 
 // 背景色から文字色を決定する関数
 // input: ex. "#ffffff"
@@ -11,6 +17,21 @@ const getTextColor = (backgroundColor: string) => {
     .map((color) => parseInt(color, 16));
   const brightness = (rgb[0] * 299 + rgb[1] * 587 + rgb[2] * 114) / 1000;
   return brightness > 125 ? "#000000" : "#ffffff";
+};
+
+const randomBgColor = () => {
+  return bgColorList[Math.floor(Math.random() * bgColorList.length)];
+};
+
+const randomInt = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const makeSentence = () => {
+  const subject = subjectList[Math.floor(Math.random() * subjectList.length)];
+  const verb = verbList[Math.floor(Math.random() * verbList.length)];
+  const object = objectList[Math.floor(Math.random() * objectList.length)];
+  return `${subject} ${verb} ${randomInt(1, 128)} ${object}.`;
 };
 
 const ToCCCPage = () => {
@@ -32,6 +53,42 @@ const ToCCCPage = () => {
             ))}
           </div>
         ))}
+      </div>
+      <br />
+      <div className="ccc-container">
+        {[...Array(10)].map(() => {
+          const bgColor = randomBgColor();
+          return (
+            <div className="row">
+              {/* {makeSentence()
+              .split(" ")
+              .map((word) => {
+                const bgColor = randomBgColor();
+                return (
+                  <div
+                    className="code-container"
+                    style={{ backgroundColor: bgColor }}
+                  >
+                    <a
+                      className="code"
+                      style={{ color: getTextColor(bgColor) }}
+                    >
+                      {word}
+                    </a>
+                  </div>
+                );
+              })} */}
+              <div
+                className="code-container"
+                style={{ backgroundColor: bgColor }}
+              >
+                <a className="code" style={{ color: getTextColor(bgColor) }}>
+                  {makeSentence()}
+                </a>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
