6749f22e5f775b4b7203e15a880a4271dab57206
shunsuke-tamura <wwwxx4869@gmail.com>
2023-12-09 03:02:25 +0900

    update: 割り切れなかった時の調整関数を引数に取るように更新

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,8 +1,13 @@
 // 配列をn個ずつに分割する
-export const splitArray = <T>(array: T[], n: number): T[][] => {
+export const splitArray = <T>(
+  array: T[],
+  n: number,
+  adjustment?: (result: T[][]) => void
+): T[][] => {
   const result = [];
   for (let i = 0; i < array.length; i += n) {
     result.push(array.slice(i, i + n));
   }
+  if (adjustment) adjustment(result);
   return result;
 };
