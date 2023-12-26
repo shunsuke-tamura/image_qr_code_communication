cd9cf1675f9595b745e7bc06c2907d7c41208430
shunsuke-tamura <wwwxx4869@gmail.com>
2023-12-26 22:48:18 +0900

    update: cqrの切り替え実装

--- src/pages/ToCQr/index.tsx
+++ src/pages/ToCQr/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { convertImageToBitArray, splitArray } from "../../common";
 import { Bit } from "../../types";
 import { cQrCellColorList, cQrCellColorRange } from "../../constants";
@@ -8,7 +8,9 @@ import FromCQr from "../fromCQr";
 
 const ToCQrPage = () => {
   const [imageFile, setImageFile] = useState<File | null>(null);
-  const [cQrContainer, setcQrContainer] = useState<JSX.Element | null>(null);
+  const [cQrContainerList, setCQrContainerList] = useState<JSX.Element[]>([]);
+  const [showingCQrContainerIndex, setShowingCQrContainerIndex] = useState(0);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
   const [binary, setBinary] = useState<Bit[]>([]);
 
   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -18,7 +20,7 @@ const ToCQrPage = () => {
     }
   };
 
-  const convertCQrFromImage = (imageBitArray: Bit[]) => {
+  const convertCQrFromImage = (imageBitArray: Bit[]): JSX.Element[] => {
     // 余った部分を0で埋める
     const adjustment = (result: Bit[][]) => {
       while (result[result.length - 1].length < cQrCellColorRange) {
@@ -33,40 +35,65 @@ const ToCQrPage = () => {
     const cQrCellColorIndexList = cQrCellBinaryList.map((cQrCellBinary) =>
       parseInt(cQrCellBinary.join(""), 2)
     );
-    setcQrContainer(
-      <div>
-        {splitArray(cQrCellColorIndexList, 6400).map(
-          (oneCQrCellColorIndexList) => (
-            <div className="cqr-container" style={{ marginTop: "15px" }}>
-              {splitArray(oneCQrCellColorIndexList, 80).map(
-                (cQrRowCellColorIndexList) => (
-                  <div className="cqr-row">
-                    {cQrRowCellColorIndexList.map((cellColorIndex) => (
-                      <div
-                        className="cqr-cell"
-                        style={{
-                          backgroundColor: cQrCellColorList[cellColorIndex],
-                        }}
-                      />
-                    ))}
-                  </div>
-                )
-              )}
-            </div>
-          )
-        )}
-      </div>
+
+    return splitArray(cQrCellColorIndexList, 6400).map(
+      (oneCQrCellColorIndexList) => (
+        <div className="cqr-container" style={{ marginTop: "15px" }}>
+          {splitArray(oneCQrCellColorIndexList, 80).map(
+            (cQrRowCellColorIndexList) => (
+              <div className="cqr-row">
+                {cQrRowCellColorIndexList.map((cellColorIndex) => (
+                  <div
+                    className="cqr-cell"
+                    style={{
+                      backgroundColor: cQrCellColorList[cellColorIndex],
+                    }}
+                  />
+                ))}
+              </div>
+            )
+          )}
+        </div>
+      )
     );
   };
+
+  const startShowingCQrCode = (cQrCodeNum: number) => {
+    console.log("start showing qr code");
+
+    const longInterval = 1000;
+    const shortInterval = 100;
+    const cQrHandler = (showIndex: number) => {
+      console.log(showIndex);
+      setShowingCQrContainerIndex(showIndex);
+      timerRef.current = setTimeout(
+        () => cQrHandler((showIndex + 1) % cQrCodeNum),
+        showIndex === 0 || showIndex === cQrCodeNum - 1
+          ? longInterval
+          : shortInterval
+      );
+    };
+    cQrHandler(0);
+  };
+
+  useEffect(() => {
+    return () => {
+      console.log("stop showing qr code");
+      if (timerRef.current) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, [timerRef]);
 
   const executeHandler = async () => {
     if (!imageFile) {
       return;
     }
     const binaryArray = await convertImageToBitArray(imageFile);
-    const cccContainer = convertCQrFromImage(binaryArray);
+    const cQrContainerList = convertCQrFromImage(binaryArray);
+    setCQrContainerList(cQrContainerList);
+    startShowingCQrCode(cQrContainerList.length);
     setBinary(binaryArray);
-    return cccContainer;
   };
 
   return (
@@ -78,7 +105,7 @@ const ToCQrPage = () => {
         <button className="primary" onClick={executeHandler}>
           Try it
         </button>
-        {cQrContainer}
+        {cQrContainerList[showingCQrContainerIndex]}
       </div>
       <br />
       <br />
