ad1d24d41ca07f7578af7fc3501b36b1d86fd15c
shunsuke-tamura <wwwxx4869@gmail.com>
2024-01-24 06:30:46 +0900

    update: reactMediaRecorderをQRコード版でも使用

--- src/pages/ToImage/index.tsx
+++ src/pages/ToImage/index.tsx
@@ -3,10 +3,17 @@ import "./style.css";
 import Webcam from "react-webcam";
 import { BrowserQRCodeReader, IScannerControls } from "@zxing/browser";
 import jsQR from "jsqr";
+import MediaRecorder from "../fromCQr/MediaRecorder";
 
 type PartData = {
   data: ImageData;
   dataStr: string; // base64
+};
+
+type CameraInfo = {
+  deviceId: string;
+  width: number;
+  height: number;
 };
 
 const ToImagePage = () => {
@@ -17,125 +24,253 @@ const ToImagePage = () => {
   const [capturing, setCapturing] = useState(false);
   const [recordedChunks, setRecordedChunks] = useState<Blob[]>([]);
   const [videoParts, setVideoParts] = useState<PartData[]>([]);
-  const controlsRef = useRef<IScannerControls | null>();
+  const codeReaderContlolsRef = useRef<IScannerControls | null>();
   const [selectedCameraId, setSelectedCameraId] = useState<string>("");
   const [cameraList, setCameraList] = useState<MediaDeviceInfo[]>([]);
   const [showImages, setShowImages] = useState<boolean>(false);
+  const [selectedCameraInfo, setSelectedCameraInfo] = useState<CameraInfo>({
+    deviceId: "",
+    width: 0,
+    height: 0,
+  });
+  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const [recording, setRecording] = useState(false);
+  const recorded = useRef<boolean>(false);
+  const [recordedBlobUrl, setRecordedBlobUrl] = useState<string>("");
+  const startMetaData = {
+    totalShowingTime: 1900,
+    interval: 50,
+    width: 720,
+    height: 1280,
+  };
+
+  const getCameraInfo = (camera: InputDeviceInfo): CameraInfo => {
+    const capabilities = camera.getCapabilities();
+    if (!capabilities.width || !capabilities.height)
+      throw new Error("cannot use this camera?");
+    const width = capabilities.width.max;
+    const height = capabilities.height.max;
+    if (!width || !height) throw new Error("cannot use this camera");
+    return { deviceId: camera.deviceId, width, height };
+  };
 
   useEffect(() => {
-    navigator.mediaDevices
-      .enumerateDevices()
-      .then((devices) => {
+    (async () => {
+      try {
+        await navigator.mediaDevices.getUserMedia({
+          video: true,
+          audio: false,
+        });
+        const devices = await navigator.mediaDevices.enumerateDevices();
         const cameras = devices.filter(
           (device) => device.kind === "videoinput"
         );
         setCameraList(cameras);
-        setSelectedCameraId(cameras[0].deviceId);
-      })
-      .catch((err) => console.log(err.name + ": " + err.message));
+        setSelectedCameraInfo(getCameraInfo(cameras[0] as InputDeviceInfo));
+        setMediaStream(
+          await navigator.mediaDevices.getUserMedia({
+            video: {
+              deviceId: {
+                exact: cameras[0].deviceId,
+              },
+            },
+            audio: false,
+          })
+        );
+      } catch (e) {
+        console.log(e);
+      }
+    })();
   }, []);
 
-  const handleDataAvailable = useCallback(
-    ({ data }: BlobEvent) => {
-      if (data.size > 0) {
-        setRecordedChunks((prev) => [...prev, data]);
-      }
-    },
-    [setRecordedChunks]
-  );
-
-  const handleStartCaptureClick = useCallback(() => {
-    if (webcamRef.current === null || webcamRef.current.stream === null) return;
-    try {
-      mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
-        mimeType: MediaRecorder.isTypeSupported("video/webm")
-          ? "video/webm"
-          : "video/mp4",
-      });
-      mediaRecorderRef.current.addEventListener(
-        "dataavailable",
-        handleDataAvailable
-      );
-      mediaRecorderRef.current.start();
-      setCapturing(true);
-    } catch (e) {
-      console.log(e);
-    }
-  }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
-
-  const handleStopCaptureClick = useCallback(() => {
-    if (mediaRecorderRef.current === null) return;
-    mediaRecorderRef.current.stop();
-    setCapturing(false);
-  }, [mediaRecorderRef]);
+  const startRecording = async () => {
+    if (recorded.current || recording) return;
+    console.log("start recording");
+    recorded.current = true;
+    setRecording(true);
+    setTimeout(() => {
+      setRecording(false);
+    }, startMetaData.totalShowingTime);
+  };
 
   useEffect(() => {
-    if (webcamRef.current === null) return;
+    if (!webcamRef.current) return;
+
+    // reinitialize MediaRecorder
+    setMediaStream(null);
+    console.log("reinitialize MediaRecorder", selectedCameraInfo);
+    setTimeout(async () => {
+      const stream = webcamRef.current!.stream;
+      if (!stream) return;
+      console.log(
+        "stream width, height",
+        stream.getVideoTracks()[0].getSettings().deviceId,
+        stream.getVideoTracks()[0].getSettings().width,
+        stream.getVideoTracks()[0].getSettings().height,
+        stream.getVideoTracks()[0].getSettings().frameRate
+      );
+      setMediaStream(stream);
+    }, 1000);
+
     const codeReader = new BrowserQRCodeReader();
     codeReader.decodeFromVideoDevice(
+      selectedCameraInfo.deviceId,
       undefined,
-      webcamRef.current.video as HTMLVideoElement,
       (result, _err, controls) => {
         if (result) {
-          if (result.getText() === "start") {
-            handleStartCaptureClick();
-          }
-          if (result.getText() === "stop") {
-            handleStopCaptureClick();
+          if (result.getText() !== "") {
+            if (result.getText() === "start") {
+              console.log("try to start recording", startMetaData);
+              startRecording();
+            }
           }
         }
-        controlsRef.current = controls;
+        codeReaderContlolsRef.current = controls;
       }
     );
     return () => {
-      if (controlsRef.current) {
-        controlsRef.current.stop();
-        controlsRef.current = null;
+      if (codeReaderContlolsRef.current) {
+        codeReaderContlolsRef.current.stop();
+        codeReaderContlolsRef.current = null;
       }
     };
-  }, [handleStartCaptureClick, handleStopCaptureClick]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedCameraInfo]);
 
-  // interval[ms]
-  const cutVideo = (interval: number) => {
+  const b64ToBlob = (base64: string) => {
+    const bin = atob(base64.replace(/^.*,/, ""));
+    const buffer = new Uint8Array(bin.length);
+    for (let i = 0; i < bin.length; i++) {
+      buffer[i] = bin.charCodeAt(i);
+    }
+    // Blobを作成
     try {
-      const video = document.createElement("video");
-      const superBuffer = new Blob(recordedChunks, {
-        type: MediaRecorder.isTypeSupported("video/webm")
-          ? "video/webm"
-          : "video/mp4",
+      const blob = new Blob([buffer.buffer], {
+        type: "image/png",
       });
-      video.src = URL.createObjectURL(superBuffer);
-      video.currentTime = Number.MAX_SAFE_INTEGER;
-      video.onloadeddata = async () => {
-        const duration = video.duration;
-        // video to image
+      return blob;
+    } catch (e) {
+      console.log("b64ToBlob", e);
+      return undefined;
+    }
+  };
+
+  const BlobToImageData = (blob: Blob) => {
+    console.log("BlobToImageData");
+    const blobUrl = URL.createObjectURL(blob);
+    return new Promise<ImageData | undefined>((resolve) => {
+      const img = new Image();
+      img.src = blobUrl;
+      img.onload = () => {
         const canvas = document.createElement("canvas");
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
+        canvas.width = img.width;
+        canvas.height = img.height;
         const ctx = canvas.getContext("2d");
         if (ctx === null) return;
-        let currentTime = 0;
-        for (;;) {
-          if (currentTime >= duration) break;
-          currentTime += interval / 1000;
-          video.currentTime = currentTime;
-          await video.play();
-          ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-          const image = ctx.getImageData(0, 0, canvas.width, canvas.height);
-          const imageStr = canvas.toDataURL("image/png");
-          setVideoParts((prev) => [
-            ...prev,
-            { data: image, dataStr: imageStr },
-          ]);
-        }
-        return video.pause();
+        ctx.drawImage(img, 0, 0);
+        const imageData = ctx.getImageData(0, 0, img.width, img.height);
+        resolve(imageData);
       };
+    });
+  };
 
-      video.load();
+  // interval[ms]
+  const cutVideo = async (interval: number) => {
+    try {
+      console.log("cutVideo");
+      const blob = await (await fetch(recordedBlobUrl)).blob();
+      const extension = blob.type.split("/")[1];
+      const dataUrl = URL.createObjectURL(blob);
+      console.log("blob", blob.size);
+      console.log("dataUrl", dataUrl);
+      console.log("process-video request");
+      const query = {
+        interval: interval,
+        extension: extension,
+        duration: startMetaData.totalShowingTime,
+      };
+      const formData = new FormData();
+      const fileObj = new File([blob], "video." + extension);
+      formData.append("video", fileObj);
+
+      const searchParam = new URLSearchParams();
+      Object.entries(query).forEach(([key, value]) => {
+        searchParam.append(key, value.toString());
+      });
+      // const baseUrl = "https://q9b0ps93-8080.asse.devtunnels.ms";
+      // const baseUrl = "http://localhost:8080";
+      const endPoint = "/api/process-video";
+      // const requestUrl = `${baseUrl}${endPoint}?${searchParam.toString()}`;
+      const requestUrl = `${endPoint}?${searchParam.toString()}`;
+      const res = await fetch(requestUrl, {
+        method: "POST",
+        body: formData,
+      });
+      console.log("res");
+      const json = await res.json();
+      console.log("process-video res", json);
+      const imageObjectList: PartData[] = [];
+      for (const imageStr of json.images as string[]) {
+        const blob = b64ToBlob(imageStr);
+        if (!blob) {
+          console.log("blob is undefined");
+          continue;
+        }
+        const imageData = await BlobToImageData(blob);
+        if (!imageData) {
+          console.log("imageData is undefined");
+          continue;
+        }
+        imageObjectList.push({ data: imageData, dataStr: imageStr });
+      }
+      setVideoParts(imageObjectList);
     } catch (e) {
       console.log(e);
     }
+    // try {
+    //   const video = document.createElement("video");
+    //   const superBuffer = new Blob(recordedChunks, {
+    //     type: MediaRecorder.isTypeSupported("video/webm")
+    //       ? "video/webm"
+    //       : "video/mp4",
+    //   });
+    //   video.src = URL.createObjectURL(superBuffer);
+    //   video.currentTime = Number.MAX_SAFE_INTEGER;
+    //   video.onloadeddata = async () => {
+    //     const duration = video.duration;
+    //     // video to image
+    //     const canvas = document.createElement("canvas");
+    //     canvas.width = video.videoWidth;
+    //     canvas.height = video.videoHeight;
+    //     const ctx = canvas.getContext("2d");
+    //     if (ctx === null) return;
+    //     let currentTime = 0;
+    //     for (;;) {
+    //       if (currentTime >= duration) break;
+    //       currentTime += interval / 1000;
+    //       video.currentTime = currentTime;
+    //       await video.play();
+    //       ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    //       const image = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    //       const imageStr = canvas.toDataURL("image/png");
+    //       setVideoParts((prev) => [
+    //         ...prev,
+    //         { data: image, dataStr: imageStr },
+    //       ]);
+    //     }
+    //     return video.pause();
+    //   };
+
+    //   video.load();
+    // } catch (e) {
+    //   console.log(e);
+    // }
   };
+
+  useEffect(() => {
+    if (recordedBlobUrl === "") return;
+    cutVideo(startMetaData.interval);
+  }, [recordedBlobUrl]);
 
   const decodeQrCode = (partData: PartData, idx: number) => {
     const code = jsQR(
@@ -155,6 +290,7 @@ const ToImagePage = () => {
     qrCodeDatas.forEach((qrCodeData) => {
       const prefix = Number(qrCodeData.substring(0, 3));
       const data = qrCodeData.substring(4);
+      console.log("prefix: ", prefix);
       if (!Number.isNaN(prefix)) {
         if (imageDatas.length <= prefix) {
           imageDatas.push(data);
@@ -183,52 +319,85 @@ const ToImagePage = () => {
     return url;
   };
 
-  const handleConvertToImage = async () => {
-    cutVideo(50);
+  const handleConvertToImage = async (videoParts: PartData[]) => {
+    // await cutVideo(50);
     // delay 2s
     // TODO cutをPromise化してawaitできるようにする
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    setVideoParts((prev) => {
-      const qrDatas: string[] = [];
-      prev.forEach((videoPart, idx) => {
-        const data = decodeQrCode(videoPart, idx);
-        if (data) {
-          qrDatas.push(data);
-        }
-      });
-      const binaryString = combineQrCodeDatas(qrDatas);
-      const binary = binaryStringToBinary(binaryString);
-      const imageDataUrl = convertBinaryToImageDataUrl(binary);
-      setImage(imageDataUrl);
-      return prev;
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
+    // setVideoParts((prev) => {
+    //   const qrDatas: string[] = [];
+    //   prev.forEach((videoPart, idx) => {
+    //     const data = decodeQrCode(videoPart, idx);
+    //     if (data) {
+    //       qrDatas.push(data);
+    //     }
+    //   });
+    //   const binaryString = combineQrCodeDatas(qrDatas);
+    //   const binary = binaryStringToBinary(binaryString);
+    //   const imageDataUrl = convertBinaryToImageDataUrl(binary);
+    //   setImage(imageDataUrl);
+    //   return prev;
+    // });
+    const qrDatas: string[] = [];
+    videoParts.forEach((videoPart, idx) => {
+      const data = decodeQrCode(videoPart, idx);
+      if (data) {
+        qrDatas.push(data);
+      }
     });
+    const binaryString = combineQrCodeDatas(qrDatas);
+    const binary = binaryStringToBinary(binaryString);
+    const imageDataUrl = convertBinaryToImageDataUrl(binary);
+    setImage(imageDataUrl);
   };
+
+  useEffect(() => {
+    if (videoParts.length === 0) return;
+    handleConvertToImage(videoParts);
+  }, [videoParts]);
 
   return (
     <div>
       <Webcam
         audio={false}
         ref={webcamRef}
-        videoConstraints={{ deviceId: selectedCameraId }}
+        videoConstraints={{ deviceId: selectedCameraInfo.deviceId }}
       ></Webcam>
       <br />
+      {mediaStream && (
+        <MediaRecorder
+          customStream={mediaStream}
+          // width={selectedCameraInfo.width}
+          // height={selectedCameraInfo.height}
+          recording={recording}
+          resultSetter={setRecordedBlobUrl}
+        />
+      )}
       <select
-        value={selectedCameraId}
-        onChange={(e) => setSelectedCameraId(e.target.value)}
+        value={cameraList.findIndex(
+          (camera) => camera.deviceId === selectedCameraInfo.deviceId
+        )}
+        onChange={(e) => {
+          setSelectedCameraInfo(
+            getCameraInfo(
+              cameraList[parseInt(e.target.value)] as InputDeviceInfo
+            )
+          );
+        }}
       >
-        {cameraList.map((camera) => (
-          <option value={camera.deviceId} key={camera.deviceId}>
+        {cameraList.map((camera, idx) => (
+          <option value={idx} key={camera.deviceId}>
             {camera.label}
           </option>
         ))}
       </select>
       <br />
-      {capturing ? (
-        <button onClick={handleStopCaptureClick} className="secondary">
+      {recording ? (
+        <button onClick={undefined} className="secondary">
           Stop
         </button>
       ) : (
-        <button onClick={handleStartCaptureClick} className="primary">
+        <button onClick={undefined} className="primary">
           Start
         </button>
       )}
@@ -254,7 +423,7 @@ const ToImagePage = () => {
         <button className="primary" onClick={() => setScan(true)}>
           Scan
         </button>
-        <button className="secondary" onClick={handleConvertToImage}>
+        <button className="secondary" onClick={undefined}>
           Convert to Image
         </button>
         <br />
